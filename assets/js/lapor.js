6c5185f00a664fdb42986cbdd7994b78fc04cbca
Fandi Adinata <09.fandisurya@gmail.com>
2023-08-30 00:40:23 +0700

    feat: pin location confirmation berfore report; add show my location function

--- assets/js/lapor.js
+++ assets/js/lapor.js
@@ -23,6 +23,49 @@ fetch('https://api.ipify.org?format=json')
     issuer = CryptoJS.SHA3(ipAddress, { outputLength: 256 }).toString(CryptoJS.enc.Hex);
 });
 
+// =========================================================================================
+// Function to show modal contain road description form
+//
+// Description:
+// Get road properties and then call mqtt connect
+// =========================================================================================
+
+function reportRoad(){
+    // update row
+    $('#modalLaporRusak').modal('show');
+    document.getElementById('nama_jalan').textContent = clickedRoad;
+
+    document.getElementById('lapor_jalan').innerHTML = `
+    <div class="col padding-left-right" id="blockchain_data">
+        <div class="row align-items-center" >
+            <div class="mx-auto">
+                <!-- Latitude -->
+                <div class="mb-3">
+                    <label class="form-label form-key primecolor">Latitude</label>
+                    <input class="form-control" id="laporLat" value="${clickedLatitude}" disabled>
+                </div>
+                <!-- Longitude -->
+                <div class="mb-3">
+                    <label class="form-label form-key primecolor">Longitude</label>
+                    <input class="form-control" id="laporLong" value="${clickedLongitude}" disabled>
+                </div>
+                <!-- Deskripsi -->
+                <div class="mb-3">
+                    <label class="form-label form-key">Deskripsi kerusakan</label>
+                    <textarea class="form-control" id="laporDesc" rows="3" placeholder="Isikan deskripsi kerusakan jalan di sini"></textarea>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+
+    document.getElementById('footer_button').innerHTML = `
+    <div class="mb-3">
+        <button type="button" class="btn btn-danger mb-3" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-success mb-3" onclick="report()">Laporkan</button>
+    </div>
+    `;
+}
 
 // =========================================================================================
 // Damaged Road Report
@@ -110,8 +153,14 @@ socket.on(clientSocket, (msg) => {
     let response=msg;
     // show the data in html
     document.getElementById("lapor_jalan").innerHTML = `
-        <div class="row primecolor">
-            <p>Laporan berhasil diunggah ke blockchain </p>
+        <div class="row primecolor" style="text-align:center">
+            <p><b>Terima kasih sudah melaporkan</b></p>
+            <br>
+            <p>
+            <span>Laporan berhasil diunggah ke blockchain.</span>
+            <br>
+            <span>Sistem akan diperbarui setelah 3-5 menit.</span>
+            </p>
             <p>Kode ticket : <a href="./laporan/info.html?tiket=${response}">${response}</a></p>
         </div>
     `;
